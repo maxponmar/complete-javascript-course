a05e55fe9308b114fec38010fc9393a6e9049336
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-27 04:07:43 -0700

    Section 2: Values and Variables.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,20 @@
 let js = 'amazing';
-if (js === 'amazing') alert("JavaScript is FUN!");
-
+// if (js === 'amazing') alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas")
+console.log(23);
+
+let firstName = "Maxiflow";
+console.log(firstName)
+
+// Variable names cannot start with numbers
+// let 3xd = 10;
+let xd = 10;
+
+// Variable names cannot use javascript keywords
+// let new = 10;
+// let function = "Hello"
+
+// By convention, constants are named with capital letters
+let PI = 3.1415;
