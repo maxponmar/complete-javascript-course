aab4adbda51013019e838fc167ebc2b7899d1379
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-27 04:11:24 -0700

    Assignments for lessons 10 and 11.

---
+++ 01-Fundamentals-Part-1/assignment.js
@@ -0,0 +1,18 @@
+// Values and Variables
+// 1 
+let country = 'Mexico'
+let contienent = 'America'
+let population = 100000;
+// 2
+console.log(country)
+console.log(contienent)
+console.log(population)
+
+
+// Data Types
+//1
+let isIsland = false;
+let language;
+//2
+console.log(isIsland)
+console.log(language)
