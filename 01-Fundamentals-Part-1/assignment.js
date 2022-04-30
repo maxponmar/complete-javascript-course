04c456c0a98ddd6e707f1751d1e5c06fcf92c857
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-30 11:53:17 -0700

    Assignments.

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -1,12 +1,12 @@
 // Values and Variables
 // 1 
-let country = 'Mexico'
-let contienent = 'America'
-let population = 100000;
+// let country = 'Mexico'
+// let contienent = 'America'
+// let population = 100000;
 // 2
-console.log(country)
-console.log(contienent)
-console.log(population)
+// console.log(country)
+// console.log(contienent)
+// console.log(population)
 
 
 // Data Types
@@ -24,3 +24,11 @@ console.log(population)
 
 let language = 'spanish';
 language = 'english';
+
+
+// Basic operators
+const population = 1000 / 2;
+population += 1;
+console.log(population);
+if (population > 6000000) console.log("In my country are more people");
+if (population < 33000000) console.log("My country has less population than the average countries.");
