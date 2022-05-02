e0e5d5651358c30cdf6b0fe0cab2080e0eda13d3
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-02 03:48:49 -0700

    Section 3: Strict Mode.

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+// Strict Mode
+'use strict';
+
+// strict mode must be the first peice of code in your javascript files.
+// Avoids to create accidental errors
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log("I can drive.");
+
+// const interface = 'Audio';
+// const private = 10;
