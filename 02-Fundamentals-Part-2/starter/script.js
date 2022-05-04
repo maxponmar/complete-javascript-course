857afd43d5c35a213c0d2ea4e8be0af52b76f12d
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-04 03:33:16 -0700

    Section 3: Introduction to Objects.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -162,14 +162,31 @@
 
 // Coding Challenge 2
 
-const calcTip = bill => bill >= 50 && bill <= 300 ?
-  bill * 0.15 : bill * 0.2;
+// const calcTip = bill => bill >= 50 && bill <= 300 ?
+//   bill * 0.15 : bill * 0.2;
 
-console.log(calcTip(100));
+// console.log(calcTip(100));
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
 
-console.log(tips);
-console.log(total);
+// console.log(tips);
+// console.log(total);
+
+// Introduction to Objects
+const jonasArray = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+}
