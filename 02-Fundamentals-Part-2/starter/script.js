3e4da41e9e7af3d7fcd81960a9c7e3cdb86a8579
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-04 16:45:23 -0700

    Section 3: The foor loop.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -233,25 +233,34 @@
 
 // Coding Challenge 3
 
-const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    return this.mass / (this.height ** 2);
-  }
-}
+// const mark = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     return this.mass / (this.height ** 2);
+//   }
+// }
 
-const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    return this.mass / (this.height ** 2);
-  }
-}
+// const john = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     return this.mass / (this.height ** 2);
+//   }
+// }
 
-if (mark.calcBMI() > john.calcBMI())
-  console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than John's (${john.calcBMI()}), weights ${mark.mass} kg and is ${mark.height} m tall.`)
-else if (john.calcBMI() > mark.calcBMI())
-  console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than Mark's (${mark.calcBMI()}), weights ${john.mass} kg and is ${john.height} m tall.`)
+// if (mark.calcBMI() > john.calcBMI())
+//   console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than John's (${john.calcBMI()}), weights ${mark.mass} kg and is ${mark.height} m tall.`)
+// else if (john.calcBMI() > mark.calcBMI())
+//   console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than Mark's (${mark.calcBMI()}), weights ${john.mass} kg and is ${john.height} m tall.`)
+
+// Iteration: The for loop
+console.log('Reperition 1')
+console.log('Reperition 2')
+console.log('Reperition 3')
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log('Reperition ' + rep)
+}
