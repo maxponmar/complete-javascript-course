943dde09a21531e87d18aed95a52db1a37978be3
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-04 17:13:18 -0700

    Section 3: The while loop.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -303,3 +303,19 @@ for (let exercise = 1; exercise < 4; exercise++) {
     console.log('Lifting weight repetition ' + rep)
   }
 }
+
+// The while loop
+let rep = 1;
+while (rep <= 10) {
+  console.log('rep ' + rep);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log('You rolled a ' + dice);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Finish");
+}
