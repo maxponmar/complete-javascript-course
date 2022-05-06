0377eef52409b2f2d03d39bbb06bfd8b3cc96730
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-06 03:29:26 -0700

    Section 7: DOM Manipulation.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.querySelector('.message').onclick = () => console.log('first')
+console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!'
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 40
