2db29a5ca98a09b20d374aa55eb5aec4e97d9f66
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-08 10:27:06 -0700

    Section 7: Pig Game, part 2.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,11 +1,33 @@
 'use strict';
 
-const scoreZeroElement = document.getElementById('score--0');
-const scoreOneElement = document.getElementById('score--1');
+const scorePlayerOneElement = document.getElementById('score--0');
+const scorePlayerTwoElement = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
 
-scoreZeroElement.textContent = 0;
-scoreOneElement.textContent = 0;
+const newButton = document.querySelector('.btn--new');
+const rollButton = document.querySelector('.btn--roll');
+const holdButton = document.querySelector('.btn--hold');
+
+const currentScorePlayerOneElement = document.getElementById('current--0');
+const currentScorePlayerTwoElement = document.getElementById('current--1');
+
+scorePlayerOneElement.textContent = 0;
+scorePlayerTwoElement.textContent = 0;
 diceElement.classList.add('hidden');
 
-let diceNumber = Math.trunc(Math.random() * 6 + 1);
+const rollDice = () => Math.trunc(Math.random() * 6 + 1);
+
+let currentScore = 0;
+
+rollButton.onclick = () => {
+  const dice = rollDice();
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    currentScorePlayerOneElement.textContent = currentScore;
+  } else {
+    currentScore = 0;
+    currentScorePlayerOneElement.textContent = 0;
+  }
+}
