b1a4b4f19925af489aeae579d20d5a26980b78ef
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-08 07:23:25 -0700

    Section7: Project 2, Modal Window Finished.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,5 +2,28 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const showModal = () => {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+btnsOpenModal.forEach((btn) => {
+  btn.onclick = showModal;
+});
+
+btnCloseModal.onclick = closeModal;
+overlay.onclick = closeModal;
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+})
