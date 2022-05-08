52b29e827f3a7a5f9eb28c4af9071025bfd11de1
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-08 10:12:24 -0700

    Section 7: Pig Game, part 1.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const scoreZeroElement = document.getElementById('score--0');
+const scoreOneElement = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+scoreZeroElement.textContent = 0;
+scoreOneElement.textContent = 0;
+diceElement.classList.add('hidden');
+
+let diceNumber = Math.trunc(Math.random() * 6 + 1);
