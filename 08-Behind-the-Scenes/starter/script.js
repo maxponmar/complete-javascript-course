6bbedd657ecb9f99c4d3cf10a82e7c6858ac5354
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-10 03:23:44 -0700

    Section 8: Scoping in Practice.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  // console.log(firstName)
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output)
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial)
+    // add(1,2);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'max';
+calcAge(1991);
