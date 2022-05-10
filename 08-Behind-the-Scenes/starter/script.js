28c80b41b5b4986a88f7fd380b5f993b28f67e4e
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-10 04:50:09 -0700

    Section 8: Primitives vs Objects.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -25,42 +25,113 @@
 // const firstName = 'max';
 // calcAge(1968);
 
-console.log(me)
-// console.log(job)
-// console.log(year)
+// console.log(me)
+// // console.log(job)
+// // console.log(year)
 
-var me = 'Jonas';
-let job = 'teacher'
-const year = 1991;
+// var me = 'Jonas';
+// let job = 'teacher'
+// const year = 1991;
 
 
-console.log(addDecl(2, 3));
-// console.log(addExpre(2, 3));
-// console.log(addArrow(2, 3));
+// console.log(addDecl(2, 3));
+// // console.log(addExpre(2, 3));
+// // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-  return a + b;
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// var addExpre = function (a, b) {
+//   return a + b;
+// }
+
+// var addArrow = (a, b) => a + b;
+
+
+// if (!numProducts) deleteShippingCart();
+// var numProducts = 10;
+
+// function deleteShippingCart() {
+//   console.log("All products deleted");
+// }
+
+// // Only on browser
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// }
+
+// calcAge(1991);
+
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// }
+
+// calcAgeArrow(12);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   }
+// }
+
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// }
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
+
+let lastName = 'ponce';
+let oldLastName = lastName;
+lastName = 'Lopez';
+console.log(lastName, oldLastName)
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27
 }
 
-var addExpre = function (a, b) {
-  return a + b;
+const marriedJesica = jessica;
+marriedJesica.lastName = 'Davis'
+
+console.log("Before marriage: ", jessica)
+console.log("After marriage: ", marriedJesica);
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob']
 }
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'EPALES';
 
-var addArrow = (a, b) => a + b;
+console.log("Before marriage: ", jessica2)
+console.log("After marriage: ", jessicaCopy);
 
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
 
-if (!numProducts) deleteShippingCart();
-var numProducts = 10;
-
-function deleteShippingCart() {
-  console.log("All products deleted");
-}
-
-// Only on browser
-var x = 1;
-let y = 2;
-const z = 3;
-
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z)
+console.log("Before marriage: ", jessica2)
+console.log("After marriage: ", jessicaCopy);
