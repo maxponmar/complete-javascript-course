a920d01809bceb478e0adad1ccca419a3ef41128
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-10 17:34:35 -0700

    Section 9: Destructuring Arrays.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,53 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+// Destructuring Arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[1];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// const [first, second] = restaurant.categories;
+// const [first, , third] = restaurant.categories;
+// console.log(first, second)
+// console.log(first, third)
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching variables
+// regular way
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// default values
+// const [p, q, r] = [8, 9];
+// console.log(p, q, r);
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
