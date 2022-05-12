75c7ba5f9a2c300cb99e268e313253fa8345dd8f
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-12 04:00:27 -0700

    Section 9: Coding Challenge 1.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,20 +54,76 @@ const rest2 = {
   owener: 'Giovanni Rossi',
 };
 
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+let [players1, players2] = game.players;
+let [gk, ...fieldPlayers] = players1;
+let allPlayers = [...players1, ...players2];
+let playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+const printGoal = function (...players) {
+  console.log(players.length);
+};
+printGoal('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoal(...game.scored);
+team1 < team2 && console.log('Team 1 is more likely to win');
+
+// Logical Assignment Operators
 // rest2.numberGuests = rest1.numGuests || 10;
 // Assign property if is falsy
 // rest1.numGuests ||= 10;
 // rest2.numGuests ||= 10;
 
 // Nulish assignment
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
-rest1.owener &&= '<Anonymous>';
-rest2.owener &&= '<Anonymous>';
+// rest1.owener &&= '<Anonymous>';
+// rest2.owener &&= '<Anonymous>';
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
 
 // The Nulish Coalescing operator
 // Nulish: null and undefined (NOT 0 or '')
