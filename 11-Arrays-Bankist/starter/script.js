586711f6742cbe6fc4033e1ff49725bb7c9e474d
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-18 05:57:04 -0700

    Section 11: Coding Challenge 3.

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -125,13 +125,24 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+// Coding Challenge 3
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+
 // Chaining Methods
 
-const toalDepositUSD = movements
-  .filter(mov => mov > 0)
-  .map(mov => mov * 1.1)
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(toalDepositUSD);
+// const toalDepositUSD = movements
+//   .filter(mov => mov > 0)
+//   .map(mov => mov * 1.1)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(toalDepositUSD);
 
 // Coding Challenge 2
 // const calcAverageHumanAge = function (ages) {
