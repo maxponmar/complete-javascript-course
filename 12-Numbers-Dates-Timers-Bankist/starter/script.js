ad19f77a695b7a4f7f0bdd1125141a2b9c5ec79b
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-19 17:57:32 -0700

    Section 12: Converting and Checking Numbers.

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -251,3 +251,27 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+console.log(23 === 23.0);
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+console.log(Number('23'));
+console.log(+'23');
+
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23'));
+
+console.log(Number.parseFloat('2.5rem'));
+console.log(Number.parseInt('2.5rem'));
+
+console.log(parseInt('2.5rem'));
+
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(23 / 0));
+
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+
+console.log(Number.isInteger('20'));
