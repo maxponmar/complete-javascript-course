8c608fc80aae12b45d6e56f088de6a6c800fd362
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-24 04:16:49 -0700

    Section 13: Slider component.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -232,6 +232,71 @@ const imageObserver = new IntersectionObserver(loadImg, {
 });
 imgTargets.forEach(img => imageObserver.observe(img));
 
+// Slider component
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
+let curSlide = 0;
+let maxSlide = slides.length;
+const createDots = () => {
+  slides.forEach((_, i) => {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+createDots();
+const activateDot = slide => {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
+const goToSlide = slide => {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${(i - slide) * 100}%)`)
+  );
+  activateDot(slide);
+};
+
+const nextSlide = () => {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  }
+  curSlide++;
+  goToSlide(curSlide);
+};
+const prevSlide = () => {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  }
+  curSlide--;
+  goToSlide(curSlide);
+};
+
+goToSlide(0);
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+});
+
+dotContainer.addEventListener('click', e => {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+  }
+});
+
 // DOM Traversing
 /*
 const h1 = document.querySelector('h1');
