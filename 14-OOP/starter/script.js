6e938c64892094a6ed76e7e166a070c2d9135ebc
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-24 16:54:31 -0700

    Section 14: Constructor functions and the new operator.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Constructor functions and the new operator
+
+const Person = function (firstName, birthYear) {
+  // instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never create methods inside constructor objects (performance)
+  // use prototypes
+  // this.calcAge = function() {
+  //   console.log(2037 - this.birthYear)
+  // }
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1.- New {} is created
+// 2.- function is called, this = {}
+// 3.- {} linked to prototype
+// 4.- function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+console.log(matilda);
+
+console.log(jonas instanceof Person);
