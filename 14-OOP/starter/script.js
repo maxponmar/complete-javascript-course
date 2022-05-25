d8e7842e6439b3e0f96ad6eecaff9a82ffb9deeb
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-25 04:42:17 -0700

    Section 14: Coding Challenge 1.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -65,3 +65,28 @@ Array.prototype.unique = function () {
 };
 
 console.log(arr.unique());
+
+// Coding challenge 1
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.brake();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
