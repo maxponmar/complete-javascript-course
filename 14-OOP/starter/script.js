2e0f852868458a225ab9dcc3ffb6a4d0435611d5
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-25 04:54:16 -0700

    Section 14: ES6 classes.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Constructor functions and the new operator
 
 const Person = function (firstName, birthYear) {
@@ -90,3 +90,37 @@ mercedes.accelerate();
 mercedes.accelerate();
 mercedes.accelerate();
 mercedes.brake();
+
+*/
+
+// ES6 Classes
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Method will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2.- Class are first-class citizens
+// 3.- Classes are executed in strict mode
