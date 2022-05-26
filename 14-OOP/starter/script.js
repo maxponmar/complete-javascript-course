283a8b63b46a2c4542a10626698ad55ea508ccc4
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-25 19:18:56 -0700

    Section 14: Inheritance between classes.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -173,6 +173,7 @@ PersonCl.hey();
 
 */
 
+/*
 const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
@@ -197,6 +198,7 @@ sarah.init('Sarah', 1979);
 console.log(sarah);
 
 // Coding Challenge 2
+
 class Car {
   constructor(make, speed) {
     this.make = make;
@@ -229,3 +231,39 @@ ford.accelerate();
 ford.brake();
 ford.speedUS = 50;
 console.log(ford);
+*/
+
+// Inheritance between classes
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  // this.firstName = firstName;
+  // this.birthYear = birthYear;
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+console.log(mike);
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
